007a815a980464b24a270fd695f5159c3aadb3d4
mineonakayama <mineo.nakayama98@gmail.com>
2020-04-13 08:52:30 +0900

    Ruby課題17、18回答

--- task.rb
+++ task.rb
@@ -148,7 +148,19 @@ end
 
 class UserQ17
   # 以下に回答を記載
+  def initialize(name:, age:, gender:)
+    @name = name
+    @age = age
+    @gender = gender
+  end
 
+  def info
+    puts <<~TEXT
+      名前：#{@name}
+      年齢：#{@age}
+      性別：#{@gender}
+    TEXT
+  end
 end
 
 def q17
@@ -163,6 +175,14 @@ end
 
 class UserQ18
   # 以下に回答を記載
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
+
+  def introduce
+    @age >= 30 ? "こんにちは，#{@name}と申します。宜しくお願いいたします。" : "はいさいまいど〜，#{@name}です！！！"
+  end
 
 end
 
